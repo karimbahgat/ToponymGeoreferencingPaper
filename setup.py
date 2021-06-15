f398a601a0e588b649250549fac30698acb9ae01
Karim Bahgat <karim.bahgat.norway@gmail.com>
2021-06-15 20:54:13 +0200

    Revert to older opencv to fix .so error

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(	long_description="Automated map georeferencing", #open("README.rst").read
 	version="""0.2.0""",
 	keywords="""map georeferencing""",
 	packages=['automap'],
-    install_requires=['Pillow', 'numpy', 'opencv-python', 'colormath', 'pytesseract',
+    install_requires=['Pillow', 'numpy', 
+					'opencv-python-headless==4.3.0.36', # see: https://github.com/opencv/opencv-python/issues/370
+					'colormath', 'pytesseract',
 					'PythonGIS @ git+https://github.com/karimbahgat/PythonGis'],
 	#dependency_links=['http://github.com/karimbahgat/PythonGis/tarball/master#egg=PythonGIS-0.3.0'],
 	classifiers=['License :: OSI Approved', 'Programming Language :: Python', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Intended Audience :: Science/Research', 'Intended Audience :: End Users/Desktop'],
