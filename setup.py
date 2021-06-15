4e7cb0f49a678affca7cc738ed3f12c057fc3caf
Karim Bahgat <karim.bahgat.norway@gmail.com>
2021-06-15 15:24:45 +0200

    Move install requirements more properly into setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ setup(	long_description="Automated map georeferencing", #open("README.rst").read
 	author="""Karim Bahgat""",
 	author_email="""karim.bahgat.norway@gmail.com""",
 	url="""http://github.com/karimbahgat/AutoMap""",
-	version="""0.1.0""",
+	version="""0.2.0""",
 	keywords="""map georeferencing""",
 	packages=['automap'],
-        requires=['requirements.txt'],
+    requires=['Pillow', 'numpy', 'opencv-python', 'colormath', 'pytesseract'],
+	dependency_links=['https://github.com/karimbahgat/PythonGis/tarball/master'],
 	classifiers=['License :: OSI Approved', 'Programming Language :: Python', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Intended Audience :: Science/Research', 'Intended Audience :: End Users/Desktop'],
 	description="""Automated map georeferencing""",
 	)
